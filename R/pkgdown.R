094a2c9e89f6c7de8244c02e6908eb5deace7842
Meera Patel <patelmeeray@gmail.com>
2020-11-21 04:15:56 -0500

    add pkgdown make functions

---
+++ R/pkgdown.R
@@ -0,0 +1,52 @@
+#' @export
+
+copyPkgdownTemplate <-
+        function() {
+                file.copy(
+                        from =
+                        system.file("templates",
+                                    "rlang_pkgdown.yml",
+                                    package = "suzyBakeOven"),
+                        to =
+                        "template_pkgdown.yml",
+                        overwrite = TRUE)
+
+        }
+
+
+#' @export
+
+makeReferenceSection <-
+        function() {
+                '
+                reference:
+                        - title: Tidy evaluation
+                        desc: >
+                                Quote arguments and expressions with unquoting support. The
+                        quosure variants wrap an environment with the expression.
+                        contents:
+                                - quo
+                                - quos
+                                - enquo
+                                - starts_with("arrange)
+                                - matches("test")
+                '
+        }
+
+
+#' @export
+
+makeReferenceList <-
+        function(reference_path) {
+
+                html_files <-
+                        list.files(path = reference_path,
+                                   pattern = "[.]{1}html$")
+
+                reference_names <-
+                        stringr::str_remove_all(string = html_files,
+                                                pattern = "[.]{1}html")
+
+                sprintf("- %s", reference_names) %>%
+                        cat(sep = "\n")
+        }
