f1f9be2037800183bef329ae0be8b15d3bc0d5f4
Meera Patel <patelmeeray@gmail.com>
2020-11-21 03:29:49 -0500

    update Package and GitHub Page

---
+++ R/declare.R
@@ -0,0 +1,36 @@
+#' @title
+#' Create R Objects from Formal Argument Default Values
+#'
+#' @description
+#' Declare formal arguments of a function along with their default values as R objects
+#'
+#' @rdname declareArgs
+#' @family declaration functions
+#' @export
+#' @importFrom secretary typewrite magentaTxt
+#' @importFrom purrr keep
+#' @importFrom rlang is_missing
+
+
+declareArgs <-
+        function(fun) {
+
+                Args <-
+                        formals(fun) %>%
+                        purrr::keep(~ !rlang::is_missing(.))
+
+                nms <- names(Args)
+                values <- unname(Args)
+
+
+                for (i in seq_along(nms)) {
+
+                        assign(
+                                x = nms[i],
+                                value = values[[i]],
+                                envir = parent.frame()
+                        )
+
+                        secretary::typewrite(secretary::magentaTxt(nms[i]), "object created.")
+                }
+        }
---
+++ R/make.R
@@ -0,0 +1,151 @@
+#' @title
+#' Make a Formal Argument Skeleton with Default Values
+#'
+#' @description
+#' Retrieve the formal arguments with the default values to copy-and-paste as formal arguments of another function. To copy-and-paste formal arguments as part of an internal function, see \code{\link{makeInternalArgs}}.
+#'
+#' @param fun  Function object.
+#'
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'        makeDefaultArgs(read.csv)
+#'  }
+#' }
+#'
+#' @rdname makeDefaultArgs
+#'
+#' @importFrom stringr str_remove_all
+#' @export
+
+makeDefaultArgs <-
+        function(fun) {
+
+                nms <- names(formals(fun))
+                values <- unname(formals(fun))
+
+                values2 <- vector()
+                for (i in seq_along(values)) {
+                        values2 <-
+                                c(values2,
+                                  deparse(values[[i]]))
+                }
+
+                mapply(paste0, nms, " = ", values2)  %>%
+                        stringr::str_remove_all(pattern = " [=]{1} $") %>%
+                        paste(collapse = ",\n") %>%
+                        cat()
+
+        }
+
+#' @title
+#' Make a Formal Argument Skeleton with Default Values
+#'
+#' @description
+#' Retrieve the formal arguments assigned to itself  to copy-and-paste as an internal function call. To copy-and-paste formal arguments with their default values, see \code{\link{makeDefaultArgs}}.
+#'
+#' @param fun PARAM_DESCRIPTION
+#'
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'        makeInternalArgs(read.csv)
+#'  }
+#' }
+#'
+#' @rdname makeInternalArgs
+#'
+#' @export
+
+makeInternalArgs <-
+        function(fun) {
+
+                nms <- names(formals(fun))
+                values <- unname(formals(fun))
+
+                mapply(paste0, nms, " = ", nms) %>%
+                        paste(collapse = ",\n") %>%
+                        cat()
+
+        }
+
+#' Make Args
+#' @export
+#' @rdname makeArgs
+#' @family make functions
+
+makeArgs <-
+        function(fun) {
+
+                makeDefaultArgs(fun = fun)
+
+                cat("\n\n")
+
+                makeInternalArgs(fun = fun)
+
+                cat("\n\n")
+
+        }
+
+
+#' Make Arg Declaration
+#' @description
+#' To automatically assign argument objects with the default value, see \code{\link[declareArgs]}
+#'
+#' @importFrom rlang is_missing
+#' @importFrom purrr keep
+#' @export
+#' @rdname makeArgDeclaration
+#' @family make functions
+
+makeArgDeclaration <-
+        function(fun) {
+
+                Args <-
+                        formals(fun) %>%
+                        purrr::keep(~ !rlang::is_missing(.))
+
+                nms <- names(Args)
+                values <- unname(Args)
+
+                values2 <- vector()
+                for (i in seq_along(values)) {
+                        values2 <-
+                                c(values2,
+                                  deparse(values[[i]]))
+                }
+
+                mapply(paste0, nms, " <- ", values2) %>%
+                        paste(collapse = "\n") %>%
+                        cat()
+
+        }
+
+
+#' Make Arg Declaration
+#' @description
+#' To automatically assign argument objects with the default value, see \code{\link[declareArgs]}
+#'
+#' @importFrom rlang is_missing
+#' @importFrom purrr keep
+#' @importFrom readr read_lines
+#' @importFrom stringr str_replace_all
+#' @export
+#' @rdname makeImports
+#' @family make functions
+#' @family DESCRIPTION functions
+
+makeImports <-
+        function() {
+
+                cat("Imports:\n")
+
+                readr::read_lines(file = "NAMESPACE") %>%
+                        grep(pattern = "import",
+                             ignore.case = TRUE,
+                             value = TRUE) %>%
+                        stringr::str_replace_all(pattern = "(^.*?[()]{1})([a-zA-Z]{1}.*?)([,)]{1})(.*)",
+                                                 replacement = "    \\2") %>%
+                        cat(sep = ",\n")
+
+        }
