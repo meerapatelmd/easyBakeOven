460584c8857a6b6c437715e0dfb9d6cbe45d72ec
Meera <patelm9@mskcc.org>
2020-12-10 11:57:24 -0500

    move function from glitter package (Issue #4)

--- R/make.R
+++ R/make.R
@@ -156,14 +156,13 @@ makeImports <-
 #' @rdname makeDescURL
 #' @family make functions
 #' @family DESCRIPTION functions
-#' @importFrom glitter makeDescriptionLinks
 
 
 makeDescURL <-
         function (github_user,
                   repo) {
 
-               glitter::makeDescriptionLinks(github_user = github_user,
+               makeDescriptionLinks(github_user = github_user,
                                              repo = repo)
 }
 
@@ -259,4 +258,32 @@ makeDescription <-
 
         }
 
+#' @title
+#' Make Links to Add to DESCRIPTION
+#'
+#' @inheritParams browse_gh
+#'
+#' @example inst/examples/packaging-make.R
+#'
+#' @rdname makeDescriptionLinks
+#' @export
 
+makeDescriptionLinks <-
+        function(github_user,
+                 repo) {
+
+                gh_pages_url <- get_gh_pages_url(github_user = github_user,
+                                                 repo = repo)
+                repo_url <- get_repo_url(github_user = github_user,
+                                         repo = repo)
+
+                issues_url <- get_issues_page_url(github_user =
+                                                          github_user,
+                                                  repo = repo)
+
+                c(URL = sprintf("URL: %s/, %s/", gh_pages_url, repo_url),
+                  BugReports = sprintf("BugReports: %s/", issues_url)) %>%
+                        paste(collapse = "\n") %>%
+                        cat()
+
+        }
