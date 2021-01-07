371424114d7f77b7207b326680f359a931cd215c
Meera Patel <patelmeeray@gmail.com>
2021-01-07 15:53:22 -0500

    bug fixes

--- R/make_new_package.R
+++ R/make_new_package.R
@@ -3,7 +3,7 @@
 #' @description
 #' Starts a new package using a designated path to a local directory, where the folder is created, README.md and NEWS.md files are added, git is initialized and linked to a remote of the same name.
 #'
-#' @inheritParams usethis::create_package
+#' @inheritParams usethis::create_tidy_package
 #' @inheritParams usethis::use_readme_md
 #' @inheritParams usethis::use_github
 #' @param spellcheck_vignettes Spell check is automatically enabled for the new package, but if TRUE, will also spell check all rmd and rnw files in the vignettes/ folder.
@@ -17,19 +17,13 @@
 make_new_package <-
   function(path,
            open = FALSE,
-           spellcheck_vignettes = TRUE,
-           initial_commit_message = "Initial commit",
-           organisation = NULL,
-           private = FALSE,
-           protocol = usethis::git_protocol(),
-           host = NULL) {
+           spellcheck_vignettes = TRUE) {
     if (!dir.exists(paths = path)) {
       usethis::create_tidy_package(path = path)
 
       current_wd <- getwd()
       on.exit(setwd(dir = current_wd))
 
-      usethis::proj_activate(path = path)
       usethis::use_package_doc(open = open)
       usethis::use_readme_md(open = open)
       usethis::use_news_md(open = open)
@@ -38,18 +32,6 @@ make_new_package <-
         utils::install.packages("spelling")
       }
       usethis::use_spell_check(vignettes = spellcheck_vignettes)
-      usethis::use_git(message = initial_commit_message)
-      usethis::git_vaccinate()
-      usethis::use_github(
-        organisation = organisation,
-        private = private,
-        protocol = protocol,
-        host = host
-      )
-      usethis::use_pipe(export = TRUE)
-      usethis::use_tibble()
-      usethis::use_tidy_eval()
-      usethis::use_tidy_style(strict = TRUE)
     } else {
       secretary::typewrite(sprintf("Path '%s' already exists.", path))
     }
@@ -61,23 +43,59 @@ make_new_package <-
 #' Setup both GitHub and tidy style settings in the usethis package.
 #' @inheritParams usethis::use_github
 #' @seealso
-#'  \code{\link[usethis]{use_git}},\code{\link[usethis]{git_vaccinate}},\code{\link[usethis]{use_github}},\code{\link[usethis]{use_pipe}},\code{\link[usethis]{use_tibble}},\code{\link[usethis]{use_tidy_github_actions}}
+#'  \code{\link[usethis]{use_git}},\code{\link[usethis]{git_vaccinate}},
+#'  \code{\link[usethis]{use_github}},\code{\link[usethis]{use_pipe}},
+#'  \code{\link[usethis]{use_tibble}},
+#'  \code{\link[usethis]{use_tidy_github_actions}}
 #' @rdname setup_tidy_git
 #' @export
-#' @importFrom usethis use_git git_vaccinate use_github use_pipe use_tibble use_tidy_eval use_tidy_style
+#' @importFrom usethis use_git git_vaccinate use_github use_pipe use_tibble
+#' use_tidy_eval use_tidy_style git_protocol
 
 setup_tidy_git <-
-  function() {
-    usethis::use_git(message = initial_commit_message)
+  function(initial_commit_message = "initial commit",
+           organisation = NULL,
+           private = FALSE,
+           protocol = usethis::git_protocol(),
+           host = NULL,
+           steps = c("use_git",
+                     "git_vaccinate",
+                     "use_github",
+                     "use_pipe",
+                     "use_tibble",
+                     "use_tidy_eval",
+                     "use_tidy_style")) {
+
+    if ("use_git" %in% steps) {
+      usethis::use_git(message = initial_commit_message)
+    }
+
+    if ("git_vaccinate" %in% steps) {
     usethis::git_vaccinate()
+    }
+
+    if ("use_github" %in% steps) {
     usethis::use_github(
       organisation = organisation,
       private = private,
       protocol = protocol,
       host = host
     )
-    usethis::use_pipe(export = TRUE)
+    }
+
+    if ("use_pipe" %in% steps) {
+    usethis::use_pipe(export = FALSE)
+    }
+
+    if ("use_tibble" %in% steps) {
     usethis::use_tibble()
+    }
+
+    if ("use_tidy_eval" %in% steps) {
     usethis::use_tidy_eval()
+    }
+
+    if ("use_tidy_style" %in% steps) {
     usethis::use_tidy_style(strict = TRUE)
+    }
   }
