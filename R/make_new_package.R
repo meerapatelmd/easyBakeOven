5eeb47e23f463150dfc2bc21f5d7bf2f9a5bf204
Meera Patel <patelmeeray@gmail.com>
2021-03-24 21:09:25 -0400

    Initial commit

--- R/make_new_package.R
+++ R/make_new_package.R
@@ -1,17 +1,30 @@
 #' @title
 #' Start a New Package
+#'
 #' @description
-#' Starts a new package using a designated path to a local directory, where the folder is created, README.md and NEWS.md files are added, git is initialized and linked to a remote of the same name.
+#' Starts a new package using a designated path to a local
+#' directory, where the folder is created, README.md and
+#' NEWS.md files are added, git is initialized and linked
+#' to a remote of the same name.
 #'
 #' @inheritParams usethis::create_tidy_package
 #' @inheritParams usethis::use_readme_md
 #' @inheritParams usethis::use_github
-#' @param spellcheck_vignettes Spell check is automatically enabled for the new package, but if TRUE, will also spell check all rmd and rnw files in the vignettes/ folder.
+#' @param spellcheck_vignettes Spell check is automatically
+#' enabled for the new package, but if TRUE, will also
+#' spellcheck all rmd and rnw files in the vignettes/ folder.
+#'
 #' @seealso
-#'  \code{\link[usethis]{create_package}},\code{\link[usethis]{proj_activate}},\code{\link[usethis]{use_readme_rmd}},\code{\link[usethis]{use_news_md}},\code{\link[usethis]{use_git}}
+#'  \code{\link[usethis]{create_package}},
+#'  \code{\link[usethis]{proj_activate}},
+#'  \code{\link[usethis]{use_readme_rmd}},
+#'  \code{\link[usethis]{use_news_md}},
+#'  \code{\link[usethis]{use_git}}
+#'
 #' @rdname make_new_package
 #' @export
-#' @importFrom usethis create_package proj_activate use_readme_md use_news_md use_git
+#' @importFrom usethis create_package proj_activate
+#' use_readme_md use_news_md use_git
 
 
 make_new_package <-
@@ -20,12 +33,15 @@ make_new_package <-
            spellcheck_vignettes = TRUE) {
     if (!dir.exists(paths = path)) {
       usethis::create_tidy_package(path = path)
+      usethis::use_git()
 
       current_wd <- getwd()
       on.exit(setwd(dir = current_wd))
 
       usethis::use_package_doc(open = open)
       usethis::use_readme_md(open = open)
+      usethis::use_code_of_conduct()
+      usethis::use_apache_license()
       usethis::use_news_md(open = open)
 
       if (!("spelling" %in% installed.packages()[, "Package"])) {
