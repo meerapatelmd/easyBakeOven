e9b13e9b96c35519709210ad0b11b153e3b8bc31
Meera Patel <patelmeeray@gmail.com>
2021-07-07 17:34:47 -0400

    deploy pkg using glitter

--- R/report.R
+++ R/report.R
@@ -8,6 +8,8 @@
 #' missing, the file will not be written. All templates are
 #' required to have an `issue_key` and `report_title`. If a report of the
 #' same issue_key and report_title already exists, it will not be overwritten.
+#' Sometimes a template will have child Rmds. See 'create_child_report()'
+#' to create template children.
 #'
 #' @param issue_key     Issue identifier.
 #' @param report_title  Name of the new report. It cannot contain any
@@ -19,6 +21,11 @@
 #' output if the feature is enabled.
 #' @param source_code_page_path Path to the GitHub repository source code
 #' for the Rmd file.
+#' @param require_all_vars Variables to replace are designated as those enclosed
+#' in curly brackets in the template Rmd file. Sometimes, the template may have
+#' variables to be called upon by the glue function iteratively outside the
+#' template itself. This argument controls whether or not all of these variables
+#' must be assigned a value or not. Default: TRUE.
 #' @param ... (Optional) Additional parameters that should be added in the
 #' front matter of the Rmd. Should be entered in the format of a named vector.
 #' @return
@@ -38,6 +45,7 @@ create_report <-
            github_page_path = "",
            source_code_page_path = "",
            template_path = system.file(package = "easyBakeOven", "reports", "Generic.Rmd"),
+           require_all_vars = TRUE,
            ...) {
 
     stopifnot(!missing(report_title))
@@ -45,7 +53,7 @@ create_report <-
 
     report_tree()
 
-    var_values <<-
+    var_values <-
     rlang::list2(project_path = project_path,
                  issue_key = issue_key,
                  report_title = report_title,
@@ -137,8 +145,14 @@ create_report <-
                                            replacement = "\\1")
     unrep_vars <- sprintf("`%s`", unrep_vars)
     unrep_vars <- paste(unrep_vars, collapse = ", ")
+
+    if (require_all_vars) {
     stop(sprintf("Missing values for vars: %s.", unrep_vars),
          call. = FALSE)
+    } else {
+      warning(sprintf("Missing values for vars: %s.", unrep_vars),
+           call. = FALSE)
+    }
   }
 
 
@@ -317,3 +331,145 @@ report_tree <-
 
   }
 
+
+#' @title
+#' Create a Child Rmd from a Template's Child
+#' @description
+#' Sometimes a template Rmd will also have children.
+#' This function makes a copy of the template child at the given `template_path`
+#' and copies it to the `child_rmd_folder` with the same filename. Unlike
+#' `create_report()`, subdirectories are not created since that is the
+#' work of the main template.
+#' @inheritParams create_report
+#' @param child_rmd_folder Path that the template will be copied to.
+#' @seealso
+#'  \code{\link[rlang]{list2}}
+#'  \code{\link[xfun]{read_utf8}}
+#'  \code{\link[stringr]{str_replace}}
+#'  \code{\link[cli]{cli_alert}}
+#' @rdname create_report_child
+#' @export
+#' @importFrom rlang list2
+#' @importFrom xfun read_utf8
+#' @importFrom stringr str_replace_all
+#' @importFrom cli cli_alert_success
+
+create_report_child <-
+  function(template_path,
+           child_rmd_folder, #Destination folder for new template to be copied
+           require_all_vars = TRUE,
+           ...) {
+
+
+    var_values <-
+      rlang::list2(...)
+
+    new_rmd_file <-
+      file.path(child_rmd_folder, basename(template_path))
+
+    if (file.exists(new_rmd_file)) {
+      stop(sprintf("`%s` already exists and was not overwritten.",
+                   new_rmd_file))
+    }
+
+    new_rmd <- xfun::read_utf8(template_path)
+    var_names <- vector()
+    for (var_num in seq_along(var_values)) {
+      var_name    <- names(var_values)[var_num]
+      var_pattern <- sprintf("[{]{1}%s[}]{1}", var_name)
+      var_value   <- var_values[[var_name]]
+      if (any(grepl(pattern = var_pattern, new_rmd))) {
+
+        new_rmd <-
+          stringr::str_replace_all(
+            string = new_rmd,
+            pattern = var_pattern,
+            replacement = var_value
+          )
+
+        var_names <-
+          c(var_names,
+            var_name)
+
+      }
+    }
+    var_values <-
+      var_values[!(names(var_values) %in% var_names)]
+
+    # Are there any user-provided variables that need to be added to the parameters?
+    if (length(var_values)>0) {
+      # Creating the lines to add under params:
+      new_params <- vector()
+      for (var_num in seq_along(var_values)) {
+        new_params <-
+          c(new_params,
+            sprintf("  %s: %s", names(var_values)[var_num], var_values[[var_num]]))
+      }
+
+
+
+      params_x <- grep("params[:]{1}",
+                       new_rmd)
+      new_rmd2 <-
+        new_rmd[1:params_x]
+
+      new_rmd2 <-
+        c(new_rmd2,
+          new_params,
+          new_rmd[(params_x+1):length(new_rmd)])
+
+      new_rmd <-
+        new_rmd2
+    }
+
+    # Are there any variables within the template that have not been replaced?
+    unrep_vars <-
+      grep(pattern = "[^```]{1}[{]{1}.*?[}]",
+           new_rmd,
+           value = TRUE,
+           perl = TRUE)
+
+    # Filtering out counts within {} in regex
+    unrep_vars <-
+      grep(pattern = "[{]{1}[0-9]{1,}[,]{0,1}[0-9]{0,}[}]{1}",
+           unrep_vars,
+           invert = TRUE,
+           value = TRUE)
+
+    if (length(unrep_vars)>0) {
+      unrep_vars <- unlist(strsplit(unrep_vars, split = "[ ]{1}"))
+      unrep_vars <- trimws(unrep_vars,
+                           which = "both")
+      unrep_vars <- grep(pattern = "^[{]{1}.*?[}]{1}$",
+                         unrep_vars,
+                         value = TRUE)
+      unrep_vars <- stringr::str_replace_all(unrep_vars,
+                                             pattern = "[{]{1}(.*?)[}]{1}",
+                                             replacement = "\\1")
+      unrep_vars <- sprintf("`%s`", unrep_vars)
+      unrep_vars <- paste(unrep_vars, collapse = ", ")
+      if (require_all_vars) {
+      stop(sprintf("Missing values for vars: %s.", unrep_vars),
+           call. = FALSE)
+      } else {
+        warning(sprintf("Missing values for vars: %s.", unrep_vars),
+                call. = FALSE)
+      }
+    }
+
+
+
+    cat(new_rmd,
+        file = new_rmd_file,
+        sep = "\n")
+
+    cli::cli_alert_success(text = new_rmd_file)
+
+
+
+    if (interactive()) {
+      file.edit(new_rmd_file)
+
+    }
+
+  }
