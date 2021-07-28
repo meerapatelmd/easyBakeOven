4ac25d4c5db1be527c21551be14d6eaa116abeb8
Meera Patel <patelmeeray@gmail.com>
2021-07-27 23:29:22 -0400

    deploy pkg using glitter

--- R/rmd_cache.R
+++ R/rmd_cache.R
@@ -1,28 +1,19 @@
 #' @title
-#' Cache Only In Interactive
-#' @description
-#' If called in an interactive session, the object will be cached.
+#' Cache an Object
 #'
 #' @param object Object to be cached.
 #' @param dirs   Required. Path to the subdirectory within R.cache root path to
 #' save the cache to.
 #'
-#' @details
-#' This function is useful when reporting on a process that has already been
-#' executed and does not require re-execution during the knitting process.
-#' For example, a Neo4j database can be written in an interactive session at
-#' which point the metadata for reporting purposes can be logged. Instead of forcing
-#' the database to be instantiated when the R markdown is knit, knitr will
-#' only pull up the cached log.
-#'
 #' @seealso
 #'  \code{\link[R.cache]{saveCache}},\code{\link[R.cache]{findCache}},\code{\link[R.cache]{loadCache}}
-#' @rdname cache_if_interactive
+#' @rdname cache_object
 #' @export
 #' @importFrom R.cache saveCache findCache loadCache
+#' @import cli
 
 
-cache_if_interactive <-
+cache_object <-
         function(object,
                  dirs,
                  overwrite = FALSE) {
@@ -31,8 +22,6 @@ cache_if_interactive <-
 
                 object_name <-
                         deparse(substitute(object))
-
-                if (interactive()) {
 
 
                         if (overwrite) {
@@ -43,7 +32,7 @@ cache_if_interactive <-
                                 key    = list(object_name)
                         )
 
-                        cli::cli_alert_success("{object_name} successfully cached.")
+                        cli::cli_alert_success("`{object_name}` successfully cached.")
 
                         } else {
 
@@ -58,7 +47,7 @@ cache_if_interactive <-
 
                                 if (object_is_cached) {
 
-                                        cli::cli_alert_danger("{object_name} already previously cached. To overwrite, set `overwrite` to TRUE.")
+                                        cli::cli_alert_danger("`{object_name}` already cached. To overwrite, set `overwrite` to TRUE.")
                                 } else {
 
                                         R.cache::saveCache(
@@ -67,14 +56,42 @@ cache_if_interactive <-
                                                 key    = list(object_name)
                                         )
 
-                                        cli::cli_alert_success("{object_name} successfully cached.")
+                                        cli::cli_alert_success("`{object_name}` successfully cached.")
 
                                 }
 
 
                         }
 
-                } else {
+
+        }
+
+
+
+#' @title
+#' Load Cached Object
+#'
+#' @param object Object that was cached.
+#' @param dirs   Required. Path to the subdirectory within R.cache root path to
+#' save the cache to.
+#'
+#' @seealso
+#'  \code{\link[R.cache]{saveCache}},\code{\link[R.cache]{findCache}},\code{\link[R.cache]{loadCache}}
+#' @rdname load_cached_object
+#' @export
+#' @importFrom R.cache saveCache findCache loadCache
+#' @import cli
+
+
+load_cached_object <-
+        function(object,
+                 dirs) {
+
+                stopifnot(!missing(dirs))
+
+                object_name <-
+                        deparse(substitute(object))
+
 
                         # Checking to see if a cache for this key already exists
                         object_is_cached <-
@@ -85,27 +102,26 @@ cache_if_interactive <-
                                         )
                                 )
 
+                        if (object_is_cached) {
 
-                        if (!object_is_cached) {
-                                stop(sprintf("`%s` is not cached at `%s`.", object_name,
-                                             cache_folder))
+                                value <-
+                                        R.cache::loadCache(
+                                                dirs   = dirs,
+                                                key    = list(object_name))
+
+                                assign(x = object_name,
+                                       value = value,
+                                       envir = parent.frame())
+
+                                cli::cli_alert_success("Cached `{object_name}` found. Loaded into parent.frame().")
+                        } else {
+
+                                cli::cli_alert_danger("`{object_name}` not cached.")
+
                         }
 
-                        value <-
-                                R.cache::loadCache(
-                                        dirs   = dirs,
-                                        key    = list(object_name))
-
-                        assign(x = object_name,
-                               value = value,
-                               envir = parent.frame())
-
-                }
 
         }
-
-
-
 
 
 
